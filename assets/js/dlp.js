7a22eb02c0d8a638350d653cf54eef978bdb50e8
Ved Topkar <vedtopkar@gmail.com>
2013-12-25 03:28:23 -0700

    Finished first version of HTML editor... success! :)

--- assets/js/dlp.js
+++ assets/js/dlp.js
@@ -2,3 +2,11 @@
 var html_editor = ace.edit("html-editor");
 html_editor.setTheme("ace/theme/solarized_dark");
 html_editor.getSession().setMode("ace/mode/html");
+
+html_editor.getSession().on('change', function(e){
+    var $frame = $('#html-output');
+    $('div#right-panel').html( $frame );
+    var doc = $frame[0].contentWindow.document;
+    var $html = $('html',doc);
+    $html.html(html_editor.getValue());
+});
