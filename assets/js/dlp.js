5e1b04c05779781eb84a2fbcd4df244e5c4258f6
Ved Topkar <vedtopkar@gmail.com>
2013-12-25 03:44:46 -0700

    Modularized html editor code

--- assets/js/dlp.js
+++ assets/js/dlp.js
@@ -1,12 +1,25 @@
-// Render ace editor for HTML editor
-var html_editor = ace.edit("html-editor");
-html_editor.setTheme("ace/theme/solarized_dark");
-html_editor.getSession().setMode("ace/mode/html");
+$(function(){
 
-html_editor.getSession().on('change', function(e){
-    var $frame = $('#html-output');
-    $('div#right-panel').html( $frame );
-    var doc = $frame[0].contentWindow.document;
-    var $html = $('html',doc);
-    $html.html(html_editor.getValue());
+    // Render ace editor for HTML editor
+    var html_editor = ace.edit("html-editor");
+    html_editor.setTheme("ace/theme/solarized_dark");
+    html_editor.getSession().setMode("ace/mode/html");
+
+    function updateHTMLOutput(){
+        var $frame = $('#html-output');
+        $('div#right-panel').html( $frame );
+        var doc = $frame[0].contentWindow.document;
+        var $html = $('html',doc);
+        $html.html(html_editor.getValue());
+    }
+
+    // render the HTML output once upon loading the page
+    updateHTMLOutput();
+
+
+    // then update the HTML output every time the input is changed
+    html_editor.getSession().on('change', function(e){
+        updateHTMLOutput();
+    });
+
 });
